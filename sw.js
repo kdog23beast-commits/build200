434875c3b88a4a5f4640173684a3e226d46f9f4d
kdog23beast-commits <kdog23beast@gmail.com>
2026-08-30 18:31:35 -0400

    Refresh PWA cache for adaptive workout update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='driverseat-workout-v3';
+const CACHE='driverseat-workout-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./C62298BA-6FFE-40CA-A7F8-BF17D81C7522.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
