08ec375b0929cfcdfaf4d206bc7e8f4bf207b81c
kdog23beast-commits <kdog23beast@gmail.com>
2026-08-30 17:35:20 -0400

    Cache Driverseat workout app icon

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE="build200-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='driverseat-workout-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./C62298BA-6FFE-40CA-A7F8-BF17D81C7522.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
